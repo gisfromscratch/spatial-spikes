ee54e8a77ebfd50c650272c34a8c40af563a9ec7
developer <gisfromscratch@live.de>
2017-05-11 22:30:56 +0200

    #2: Simple parsing of lat and lon

--- src/python/main.py
+++ src/python/main.py
@@ -7,5 +7,14 @@ if __name__ == "__main__":
 		lineNumber = 0
 		for line in geonamesFile:
 			lineNumber +=1
+			tokens = line.split("\t")
+			if (6 < len(tokens)):
+				id = int(tokens[0])
+				lat = float(tokens[4])
+				lon = float(tokens[5])
+				if (lat < -90 or 90 < lat):
+					print "Feature %d has an invalid latitude!"
+				if (lon < -180 or 180 < lon):
+					print "Feature %d has an invalid longitude!"
 
 		print "%d lines read" % lineNumber
