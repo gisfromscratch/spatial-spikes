03fd2d40a34ff2ccdfb50fbcbdd724d33091f549
developer <gisfromscratch@live.de>
2017-05-11 20:24:24 +0200

    #1: Read the file and count the lines using Python

--- src/python/main.py
+++ src/python/main.py
@@ -3,6 +3,9 @@
 
 if __name__ == "__main__":
 	print "Geonames"
-	with open("../data/cities15000.txt") as geonamesFile:
+	with open("../../data/cities15000.txt") as geonamesFile:
+		lineNumber = 0
 		for line in geonamesFile:
-			print line
+			lineNumber +=1
+
+		print "%d lines read" % lineNumber
