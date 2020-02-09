855840f4aa63bbc2f9591a71dc03ca29850cbdd7
J.T <gisfromscratch@live.de>
2020-02-09 16:39:32 +0100

    #3 Rename class

--- src/java/geojson-shell/geojson.shell/src/geojson/shell/GeoJsonShell.java
+++ src/java/geojson-shell/geojson.shell/src/geojson/shell/GeoJsonShell.java
@@ -1,7 +1,7 @@
 package geojson.shell;
 
-public class App {
+public class GeoJsonShell {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        
     }
 }
