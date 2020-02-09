9cc0bd793f91554b08338701d904201a4e812baa
J.T <gisfromscratch@live.de>
2020-02-09 17:13:40 +0100

    #3 Added the jackson JSON parser

--- src/java/geojson.shell/src/geojson/shell/GeoJsonShell.java
+++ src/java/geojson.shell/src/geojson/shell/GeoJsonShell.java
@@ -1,7 +1,14 @@
 package geojson.shell;
 
+import com.esri.core.geometry.GeoJsonImportFlags;
+import com.esri.core.geometry.Geometry;
+import com.esri.core.geometry.GeometryEngine;
+import com.esri.core.geometry.MapGeometry;
+
 public class GeoJsonShell {
     public static void main(String[] args) throws Exception {
+        String geoJson = "{ \"type\": \"Point\", \"coordinates\": [ 1.53414, 42.50729 ] }";
+        MapGeometry mapGeometry = GeometryEngine.geoJsonToGeometry(geoJson, GeoJsonImportFlags.geoJsonImportDefaults, Geometry.Type.Point);
         
     }
 }
