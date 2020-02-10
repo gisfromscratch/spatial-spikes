905ad3474a247ca722517cded99d26a250bd69c2
J.T <gisfromscratch@live.de>
2020-02-10 06:28:09 +0100

    #3 Calculate the intersections

--- src/java/geojson.shell/src/geojson/shell/GeoJsonShell.java
+++ src/java/geojson.shell/src/geojson/shell/GeoJsonShell.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import com.esri.core.geometry.GeoJsonImportFlags;
 import com.esri.core.geometry.Geometry;
@@ -19,7 +21,7 @@ public class GeoJsonShell {
     private static List<MapGeometry> readGeometriesFromGeoJsonFile(String filePath, Geometry.Type geometryType)
             throws IOException
     {
-        ArrayList<MapGeometry> geometries = new ArrayList<MapGeometry>();
+        ArrayList<MapGeometry> geometries = new ArrayList<>();
         byte[] geoJsonData = Files.readAllBytes(Paths.get(filePath));
 
         ObjectMapper mapper = new ObjectMapper();
@@ -37,23 +39,37 @@ public class GeoJsonShell {
         return geometries;
     }
 
-    public static void main(String[] args) throws Exception {
-        int numberOfMismatches = 0;
-        List<MapGeometry> cities = readGeometriesFromGeoJsonFile("../../../data/world_cities1000_sample.geojson", Geometry.Type.Point);
-        List<MapGeometry> countries = readGeometriesFromGeoJsonFile("../../../data/world_countries_sample.geojson", Geometry.Type.Polygon);
+    private static Map<MapGeometry, List<MapGeometry>> intersect(List<MapGeometry> countries, List<MapGeometry> cities) {
+        HashMap<MapGeometry, List<MapGeometry>> intersections = new HashMap<>(cities.size());
         Iterator<MapGeometry> cityElements = cities.iterator();
         while (cityElements.hasNext()) {
             MapGeometry city = cityElements.next();
+            ArrayList<MapGeometry> countryIntersections = new ArrayList<>();
             Iterator<MapGeometry> countryElements = countries.iterator();
             while (countryElements.hasNext()) {
                 MapGeometry country = countryElements.next();
                 Geometry intersection = GeometryEngine.intersect(country.getGeometry(), city.getGeometry(), city.getSpatialReference());
-                if (intersection.isEmpty()) {
-                    numberOfMismatches++;
+                if (!intersection.isEmpty()) {
+                    countryIntersections.add(country);
                 }
             }
+            intersections.put(city, countryIntersections);
         }
 
+        return intersections;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int numberOfMismatches = 0;
+        List<MapGeometry> cities = readGeometriesFromGeoJsonFile("../../../data/world_cities1000_sample.geojson", Geometry.Type.Point);
+        List<MapGeometry> countries = readGeometriesFromGeoJsonFile("../../../data/world_countries_sample.geojson", Geometry.Type.Polygon);
+        Map<MapGeometry, List<MapGeometry>> intersections = intersect(countries, cities);
+        for (Map.Entry<MapGeometry, List<MapGeometry>> intersection : intersections.entrySet()) {
+            List<MapGeometry> countryIntersections = intersection.getValue();
+            if (countryIntersections.isEmpty()) {
+                numberOfMismatches++;
+            }
+        }
         System.out.println(numberOfMismatches);
     }
 }
